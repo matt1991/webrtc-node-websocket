a01525cdfe8cdc8f47705928102b8e5d044fa9ed
Matt MIAO <matt.miao@digitalcube.com>
2016-05-10 14:18:02 +0800

    i

--- server.js
+++ server.js
@@ -42,10 +42,10 @@ var query = req.query;
 	        password:password,
 	        ttl:time_to_live,
 	        "urls": [
-	          "turn:104.236.154.197:3478?transport=udp",
-	          "turn:104.236.154.197:3478?transport=tcp",
-	          "turn:104.236.154.197:3479?transport=udp",
-	          "turn:104.236.154.197:3479?transport=tcp"
+	          "turn:104.155.208.138:3478?transport=udp",
+	          "turn:104.155.208.138:3478?transport=tcp",
+	          "turn:104.155.208.138:3479?transport=udp",
+	          "turn:104.155.208.138:3479?transport=tcp"
 	            ]
 	    });
 	}
